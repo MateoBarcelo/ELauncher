420d8fb51fd79a13d0f16223a54d3cf91a174eb0
MateoBarcelo <mateobarcelo88@gmail.com>
2025-04-10 12:42:07 -0300

    feat: integrate XMCL for Minecraft version management and installation
    - Added dependencies for @xmcl/core and @xmcl/installer to package.json.
    - Refactored main.ts to utilize XMCL for downloading and launching Minecraft.
    - Implemented mod copying functionality in a new utility file mod-extractor.ts.
    - Enhanced logging and error handling during the installation process.

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,30 @@
 import { app, BrowserWindow, ipcMain as emitter } from "electron";
-import log from "electron-log/main"
-import { Client, Authenticator } from "minecraft-launcher-core";
+import log from "electron-log/main";
 import { Auth } from "msmc";
+import {
+  getVersionList,
+  MinecraftVersion,
+  install,
+  installFabric,
+  getFabricArtifacts,
+  FabricArtifacts,
+  FabricArtifactVersion,
+  FabricLoaderArtifact,
+  getFabricLoaderArtifact,
+  installDependencies,
+} from "@xmcl/installer";
+import { launch, MinecraftLocation, Version } from "@xmcl/core";
+import { GAME_FOLDER } from "./utils/locations";
+import { copyMods } from "./utils/mod-extractor";
+import { Agent } from "undici";
+import { loadFabric } from "./loaders/fabric";
 
-const authManager = new Auth("select_account")
+const authManager = new Auth("select_account");
 
+const GAME_VERSION = "1.19.2";
 
-log.initialize()
-log.info('Log from the main process');
+log.initialize();
+log.info("Log from the main process");
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
@@ -19,47 +36,69 @@ const createWindow = () => {
   });
 
   win.loadFile("./index.html");
-}
+};
 
 app.whenReady().then(createWindow);
 
 emitter.on("launch-app", async () => {
-  await startLauncher()
-})
+  await startTest();
+});
 
-async function startLauncher() {
+async function startTest() {
+  const list: MinecraftVersion[] = (await getVersionList()).versions;
+  const aVersion: MinecraftVersion =
+    list.find((v) => v.id == GAME_VERSION) || list[0]; // i just pick the first version in list here
 
-  const xboxManager = await authManager.launch("raw")
+  const xboxManager = await authManager.launch("raw");
 
   const token = await xboxManager.getMinecraft();
 
-  if(!token || !token.profile) {
-    return console.log('Failed to get Minecraft token')
+  if (!token || !token.profile) {
+    return console.log("Failed to get Minecraft token");
   }
 
-  const launcher = new Client();
-  
-  let opts = {
-      // For production launchers, I recommend not passing 
-      // the getAuth function through the authorization field and instead
-      // handling authentication outside before you initialize
-      // MCLC so you can handle auth based errors and validation!
-      authorization: token.mclc(),
-      root: "./minecraft",
-      version: {
-          number: "1.19.3",
-          type: "release"
+  //This download agent is so important, without it, the download will be very slow and sometimes fail
+  const agent = new Agent({
+    connections: 8,
+  });
+
+  console.log("Downloading Minecraft", aVersion.id);
+  await install(aVersion, GAME_FOLDER, {
+    agent: {
+      //@ts-ignore
+      dispatcher: agent,
+    },
+  });
+  console.log("Installed Minecraft");
+  copyMods();
+  const gameVersion = await loadFabric(GAME_VERSION);
+
+  if (!gameVersion) {
+    return console.log("Failed to install fabric loader");
+  }
+
+  try {
+    await installDependencies(await Version.parse(GAME_FOLDER, gameVersion), {
+      agent: {
+        //@ts-ignore
+        dispatcher: agent,
       },
-      memory: {
-          max: "6G",
-          min: "4G"
-      }
+    });
+  } catch (e) {
+    console.log("Failed to install dependencies", e);
+    return;
   }
-  
-  //@ts-ignore
-  launcher.launch(opts);
-  
-  launcher.on('debug', (e) => console.log(e));
-  launcher.on('data', (e) => console.log(e));
 
+  console.log("Installed dependencies");
+  await launch({
+    accessToken: token.mcToken,
+    gameProfile: {
+      id: token.profile.id,
+      name: token.profile.name,
+    },
+    version: gameVersion,
+    gamePath: GAME_FOLDER,
+    javaPath: "C:/Program Files/Java/jdk-21/bin/java.exe",
+  });
+  console.log("Installed");
 }
---
+++ src/utils/mod-extractor.ts
@@ -0,0 +1,31 @@
+import path from "path";
+import fs from "fs";
+import os from "os";
+import process from "process";
+import { MODS_FOLDER } from "./locations";
+
+const destinationPath = MODS_FOLDER;
+const sourcePath = path.join(process.cwd(), "./src/resources", "mods"); //path.join(process.resourcesPath, "mods");
+
+const createDestinationPath = () => {
+  if (!fs.existsSync(destinationPath)) {
+    fs.mkdirSync(destinationPath, { recursive: true });
+  }
+};
+
+export const copyMods = () => {
+  createDestinationPath();
+  console.log("Copying mods from resources to mods folder...");
+  console.log(`Source path: ${sourcePath}`);
+  console.log(`Destination path: ${destinationPath}`);
+
+  const files = fs.readdirSync(sourcePath);
+  files.forEach((file) => {
+    const sourceFile = path.join(sourcePath, file);
+    const destFile = path.join(destinationPath, file);
+    if (!fs.existsSync(destFile)) {
+      fs.copyFileSync(sourceFile, destFile);
+      console.log(`Copied ${file} to ${destinationPath}`);
+    }
+  });
+};
