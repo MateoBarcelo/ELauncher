620f4afa9c213b80b5430e6857584a9730772a0f
MateoBarcelo <mateobarcelo88@gmail.com>
2025-04-11 14:28:46 -0300

    feat: add loadForge function for installing Minecraft Forge

---
+++ src/loaders/forge.ts
@@ -0,0 +1,29 @@
+import {
+  installForge,
+  getForgeVersionList,
+  ForgeVersionList,
+  ForgeVersion,
+} from "@xmcl/installer";
+import { GAME_FOLDER } from "../utils/locations";
+import { agent } from "../main";
+
+export async function loadForge(version: string): Promise<string | undefined> {
+  const list: ForgeVersionList = await getForgeVersionList({
+    minecraft: version,
+  });
+  console.log("Forge version list: ", list.versions, list.mcversion);
+
+  const versionToInstall: ForgeVersion =
+    list.versions.find((ver) => ver.mcversion === version) || list.versions[0];
+
+  console.log("Forge version to install: ", versionToInstall.version);
+
+  const gameVersion = await installForge(versionToInstall, GAME_FOLDER, {
+    agent: {
+      //@ts-ignore
+      dispatcher: agent,
+    },
+  });
+
+  return gameVersion;
+}
