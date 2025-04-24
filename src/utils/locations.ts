65672320a64f24fa72f0192c91d0f709cfb4e2f3
MateoBarcelo <mateobarcelo88@gmail.com>
2025-04-24 12:38:59 -0300

    fix: update project name to E Launcher in HTML and locations

--- src/utils/locations.ts
+++ src/utils/locations.ts
@@ -3,7 +3,7 @@ import path from "path";
 
 export const ROOT_FOLDER = "./"; //path.join(os.homedir(), "AppData", "Roaming");
 
-export const GAME_FOLDER = path.join(ROOT_FOLDER, ".creatia");
+export const GAME_FOLDER = path.join(ROOT_FOLDER, ".elauncher");
 
 export const MODS_FOLDER = path.join(GAME_FOLDER, "mods");
 export const SAVES_FOLDER = path.join(GAME_FOLDER, "saves");
